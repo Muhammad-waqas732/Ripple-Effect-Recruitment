a783c6edbb70014af079b9d0e320d1108ce66331
Muhammad-waqas732 <63126546+Muhammad-waqas732@users.noreply.github.com>
2022-07-08 14:59:16 +0500

    Update iframe.js

--- iframe.js
+++ iframe.js
@@ -1,3 +1,3 @@
 let link = document.createElement("style");
 link.innerHTML = "app-sidebar{display:none;}.mobile-filter-container{display:none;}";
-frames.document.head.appendChild(link2222);
+frames.document.head.appendChild(link);
